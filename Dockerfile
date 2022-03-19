9bafe8cfdcefe07419da713d6495b4d0985225d1
zengjinliu <zengjinliu_job@163.com>
2022-03-19 20:44:35 +0800

    docker

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM java8
-MAINTAINER Minte
-COPY my-app-0.0.1-SNAPSHOT.jar /usr/local/my-app-0.0.1-SNAPSHOT.jar
-EXPOSE 8090
-ENTRYPOINT ["java","-jar","/usr/local/my-app-0.0.1-SNAPSHOT.jar"]
-
-
--- src/main/java/com/mycompany/app/MyAppApplication.java
+++ src/main/java/com/mycompany/app/MyAppApplication.java
@@ -10,6 +10,8 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class MyAppApplication {
     public static void main(String[] args) {
+
         SpringApplication.run(MyAppApplication.class, args);
     }
+
 }
